afc5781e5e21233ef7c9eba83403da6a96942468
Jide Fajobi <wazogear@gmail.com>
2013-06-15 21:21:28 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MnM::Application.config.secret_token = '7411ee4679448c1b7c6e8b846117c9f4277c0759c84e043670d86672f5886cd044ff558cb418616269f422c14670476b3395ec889d429c8f699d7c8b2c16dd12'
