8244adc9c2cedfb639992349e5fbda8d51801a7f
Jide Fajobi <wazogear@gmail.com>
2013-06-17 11:29:35 -0400

    create test for homepage

--- spec/features/user_hits_homepage_spec.rb
+++ spec/features/user_hits_homepage_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe 'user enters homepage' do
-  it 'shows gives a user a link to join sign in/up' do
+  it 'gives a user a link to join sign in/up' do
     visit '/'
     expect(page).to have_content('Sign in To Discover New Restaurants')
+    expect(page).to have_content('Log In')
+    expect(page).to have_content('Sign Up')
+    expect(page).to have_content('Try it Out!')
+    expect(page).to have_content('Contact Us')
+    expect(page).to have_content('Myster Meals')
   end
 end
--- spec/features/user_signs_up_for_website_spec.rb
+++ spec/features/user_signs_up_for_website_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe 'Signing up for Mystery Meals' do 
-  it 'it has all the forms needed to make a new account' do 
+  it 'has all the forms needed to make a new account' do 
+    visit '/'
+    click_link 'Sign in To Discover New Restaurants'
+    expect(page).to have_content('username')
+    expect(page).to have_content('email')
   end
 end
