cbba413ef936d4c4364ed204fd1253d9ca73a53d
Jide Fajobi <wazogear@gmail.com>
2013-06-19 11:34:27 -0400

    finish basic homepage one broken test

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ end
 
 group :test do 
   gem 'factory_girl_rails'
+  gem "shoulda-matchers"
 end
 
 gem 'jquery-rails'
--- spec/features/user_signs_up_for_website_spec.rb
+++ spec/features/user_signs_up_for_website_spec.rb
@@ -3,8 +3,19 @@ require 'spec_helper'
 describe 'Signing up for Mystery Meals' do 
   it 'has all the forms needed to make a new account' do 
     visit '/'
-    click_link 'Sign in To Discover New Restaurants'
-    expect(page).to have_content('username')
-    expect(page).to have_content('email')
+    click_link 'Sign Up'
+    expect(page).to have_content('Password')
+    expect(page).to have_content('Email')
   end
+
+  it 'Creates a new user when you fill in the forms' do 
+    prev_count = User.count
+    visit '/users/sign_up'
+    fill_in 'Email', with: 'joejoe@m.com'
+    fill_in 'Password', with: '12345asdf67'
+    fill_in 'Password confirmation', with: '12345asdf67'
+    click_button 'Sign up'
+    expect(User.count).to eql(prev_count + 1)
+  end
+
 end
