551e91ae26b10596fd27499821f19087794ea544
Jide Fajobi <wazogear@gmail.com>
2013-06-18 23:00:58 -0400

    add links to homepage

--- spec/features/user_hits_homepage_spec.rb
+++ spec/features/user_hits_homepage_spec.rb
@@ -3,11 +3,10 @@ require 'spec_helper'
 describe 'user enters homepage' do
   it 'gives a user a link to join sign in/up' do
     visit '/'
-    expect(page).to have_content('Sign in To Discover New Restaurants')
     expect(page).to have_content('Log In')
     expect(page).to have_content('Sign Up')
     expect(page).to have_content('Try it Out!')
     expect(page).to have_content('Contact Us')
-    expect(page).to have_content('Myster Meals')
+    expect(page).to have_content('Mystery Meals')
   end
 end
